e0baa05b20b316c2e3c3f374df14ef669fa0e9bf
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-08-20 18:52:34 -0400

    more components

--- Scoreboard/src/components/App.js
+++ Scoreboard/src/components/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Player from "./Player";
+import AddPlayerForm from "./AddPlayerForm";
 
 const App = () => {
-  const [players, setPlayers] = React.useState([
+  const [players, setPlayers] = useState([
     {
       name: "Bob",
       score: 0,
@@ -26,6 +27,8 @@ const App = () => {
     },
   ]);
 
+  const [nextPlayerId, setNextPlayerId] = useState(5);
+
   const handleRemovePlayer = (id) => {
     setPlayers((prevPlayers) => prevPlayers.filter((p) => p.id !== id));
   };
@@ -45,9 +48,21 @@ const App = () => {
     );
   };
 
+  const handleAddPlayer = (name) => {
+    setPlayers((prevPlayers) => [
+      ...prevPlayers,
+      {
+        name,
+        score: 0,
+        id: nextPlayerId,
+      },
+    ]);
+    setNextPlayerId((prevId) => prevId + 1);
+  };
+
   return (
     <div className="scoreboard">
-      <Header title="Scoreboard" totalPlayers={players.length} />
+      <Header title="Scoreboard" players={players} />
 
       {/* Players list */}
       {players.map((player) => (
@@ -60,6 +75,7 @@ const App = () => {
           changeScore={handleScoreChange}
         />
       ))}
+      <AddPlayerForm addPlayer={handleAddPlayer} />
     </div>
   );
 };
