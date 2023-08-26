782b10504e18101f922fc36097cbf23ccfa7a6cf
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-08-26 18:38:11 -0400

    creating routes

--- React-Router-Practice/src/App.js
+++ React-Router-Practice/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+// components
+import Home from "./components/Home";
+import About from "./components/About";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container">
-
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
