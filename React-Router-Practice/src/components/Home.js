f19db45779fd7380cb15d62f2b9bf13e8be2f989
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-09-07 13:16:53 -0400

    refactoring
    just added errors though

--- React-Router-Practice/src/components/Home.js
+++ React-Router-Practice/src/components/Home.js
@@ -1,7 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const name = useRef();
+  const topic = useRef();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    let teacherName = name.current.value;
+    let teacherTopic = topic.current.value;
+    let path = `teachers/${teacherTopic}/${teacherName}`;
+    navigate(path);
+  };
+
   return (
     <div className="main-content home">
       <h2>Front End Course Directory</h2>
@@ -23,7 +35,11 @@ const Home = () => {
       <hr />
 
       <h3>Featured Teachers</h3>
-      <Link to="teachers/HTML/Tommy-Wingo">Tommy Wingo</Link>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" ref={name} />
+        <input type="text" placeholder="Topic" ref={topic} />
+        <button type="submit">Go!</button>
+      </form>
     </div>
   );
 };
